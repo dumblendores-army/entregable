3360b27f31dc2657c299a89a96ce8519c2c2c976
j0sete <j0sete@localhost.localdomain>
2017-05-20 21:45:55 +0200

    Cambio prioridades y hebras

--- practica2.c
+++ practica2.c
@@ -1,3 +1,28 @@
+/**
+*	@auth 	 Jose Manuel Rodriguez Montes
+*	@version 0.1, 20 de Mayo de 2017
+*	
+*	Ejercicio practico para la asignatura de Sistemas Operativos en Tiempo Real(SOTR)\
+*	de la Universidad de Malaga(UMA).
+*	<p>
+*	El ejercicio consiste en un sistema de monitoriacion de temperatura y presion. Se\
+*	hace uso de sistemas de sincronicacion tales como mutex y timers(clock_nanosleep)\
+*	para su correcta ejecucion.
+*	Se van a utilizar 5 hebras, cuyas prioridades se han asignado segun su tiempo de\
+*	ejecucion.
+*	<p>
+*	La documentacion se mostrara en ingles y español segun el nivel de aclaracion del\
+*	contenido sea necesario. Las funciones y variables tambien se usaran en español e\
+*	ingles(nombres de funcion de hebras en ingles, nombre de los pthread_t en español).\ 
+*	Los comentarios mostrados por pantalla seran en castellano. SE OMITIRA EL USO DE\
+*	TILDES.
+*	<p>
+*	El programa esta pensado para ser ejecutado en una maquina con nucleo Linux y un\
+*	procesador con un solo nucleo. Las pruebas se han realizado en un sistema\
+*	operativo Linux, distribucion Feroda 25 LXDE DESKTOP con version kernel:\
+*	4.10.15-200.fc25.x86_64
+*
+*/
 #include <stdio.h>
 #include <unistd.h>
 #define __USE_UNIX98
@@ -14,22 +39,27 @@
 // ==============================
 // === DEFINES ==================
 // ==============================
-#define ACT_TEMP   0x02 // 0x0010
-#define ACT_PRES   0x01 // 0x0001
-#define DEACT_TEMP 0x0D // 0x1101
-#define DEACT_PRES 0x0E // 0x1110
-#define TEMP_MASK  0x02 // 0x0010
-#define PRES_MASK  0x01 // 0x0001
+#define ACT_TEMP   0x02 // 0010 - Bit 1 identificador de estado actuador presion
+#define ACT_PRES   0x01 // 0001 - Bit 0 identificador de estado actuador presion
+#define DEACT_TEMP 0x0D // 1101
+#define DEACT_PRES 0x0E // 1110
+#define TEMP_MASK  0x02 // 0010
+#define PRES_MASK  0x01 // 0001
 
 #define MAX_PRESSURE    1000
 #define MIN_PRESSURE    900
 #define MAX_TEMPERATURE 100
 #define MIN_TEMPERATURE 90
 
-#define ERROR_SET_PROTOCOL    0x01
-#define ERROR_SET_PRIOCEILING 0x02
-#define ERROR_CLOCK_GETTIME   0x03
-#define ERROR_CLOCK_NANOSLEEP 0x04
+#define ERROR_SET_PROTOCOL     0x01 // Codigo de errores, usar la funcion {@link #error(error_t error_code) error}
+#define ERROR_SET_PRIOCEILING  0x02
+#define ERROR_CLOCK_GETTIME    0x03
+#define ERROR_CLOCK_NANOSLEEP  0x04
+#define ERROR_PTRD_ATTR_INIT   0x05
+#define ERROR_PTRD_ATTR_SETINH 0x06
+#define ERROR_PTRD_ATTR_SETPOL 0x07
+#define ERROR_PTRD_SETSCHED	   0x08
+#define ERROR_PTRD_CREATE	   0x09
 
 
 
@@ -90,8 +120,7 @@ void ms_espera_activa( int tiempo) {
 *
 *	@param tm variable de tiempo a normalizar
 */
-static void normalizar(struct timespec* tm)
-{
+static void normalizar(struct timespec* tm) {
     if (tm->tv_nsec >= 1000000000L) {
         tm->tv_sec += (tm->tv_nsec / 1000000000L);
         tm->tv_nsec = (tm->tv_nsec % 1000000000L);
@@ -110,12 +139,27 @@ static void normalizar(struct timespec* tm)
            ||(tm->tv_sec<0 && tm->tv_nsec>-1000000000L && tm->tv_nsec<=0));
 }
 
+/*
+*	Muestra por pantalla la hora actual.
+*
+*/
+void get_actual_time() {
+	time_t rawtime;
+	struct tm * timeinfo;
+
+	time( &rawtime);
+	timeinfo = localtime( &rawtime);
+	printf ("\n%s::", asctime(timeinfo) );
+}
+
 /**
-*	Funcion para mostrar errores por pantalla
+*	Funcion para mostrar errores por pantalla.
 *   
-*	@see Codigo de errores en linea 27
+*	@see Codigo de errores en linea 54
 */
 void error(error_t error_code) {
+
+	get_actual_time();
 	
 	switch(error_code) {
 		case ERROR_SET_PROTOCOL:
@@ -130,12 +174,27 @@ void error(error_t error_code) {
 		case ERROR_CLOCK_NANOSLEEP:
 			printf("ERROR en __clock_nanosleep\n");
 			break;
+		case ERROR_PTRD_ATTR_INIT:
+			printf("ERROR en __pthread_attr_init\n");
+			break;
+		case ERROR_PTRD_ATTR_SETINH:
+			printf("ERROR en __pthread_attr_setinheritsched\n");
+			break;
+		case ERROR_PTRD_ATTR_SETPOL:
+			printf("ERROR en __pthread_attr_setschedpolicy\n");
+			break;
+		case ERROR_PTRD_SETSCHED:
+			printf("ERROR en __pthread_attr_setschedparam\n");
+			break;
+		case ERROR_PTRD_CREATE:
+			printf("ERROR en __pthread_create\n");
+			break;
 		default:
 			printf("ERROR UNKOWN\n");
 
 	}
 
-	printf("Finish the execution\n");
+	printf("Finish the execution...\n");
 	exit(-1);
 }
 
@@ -147,8 +206,54 @@ void error(error_t error_code) {
 
 int main() {
 
+	pthread_t monitor;
+	pthread_t control_presion, control_temperatura;
+	pthread_t sensor_presion, sensor_temperatura;
+
+	pthread_attr_t attr;
+	struct sched_param prio;
+
 	init_station();
 	configure_mutex();
+
+	// THREADS
+	// Configurando attr
+	if( pthread_attr_init( &attr) != 0) 								   error(ERROR_PTRD_ATTR_INIT);
+	if( pthread_attr_setschedpolicy( &attr, SCHED_FIFO) != 0) 			   error(ERROR_PTRD_ATTR_SETPOL);
+	if( pthread_attr_setinheritsched( &attr, PTHREAD_EXPLICIT_SCHED) != 0) error(ERROR_PTRD_ATTR_SETINH);
+	
+	// Configurando las prioridades de las hebras y lanzandolas
+
+	// #M
+	prio.sched_priority = 1;
+	if( pthread_attr_setschedparam(&attr, &prio) != 0) 	        error(ERROR_PTRD_SETSCHED);
+	if( pthread_create(&monitor, &attr, monitoring, NULL) != 0) error(ERROR_PTRD_CREATE);
+
+	// #Ct
+	prio.sched_priority = 2;
+	if( pthread_attr_setschedparam(&attr, &prio) != 0) 	   						     error(ERROR_PTRD_SETSCHED);
+	if( pthread_create(&control_temperatura, &attr, temperature_control, NULL) != 0) error(ERROR_PTRD_CREATE);
+
+	// #St
+	prio.sched_priority = 3;
+	if( pthread_attr_setschedparam(&attr, &prio) != 0) 	  						   error(ERROR_PTRD_SETSCHED);
+	if( pthread_create(&sensor_temperatura, &attr, temperature_sensor, NULL) != 0) error(ERROR_PTRD_CREATE);
+
+	// #Cp
+	prio.sched_priority = 4;
+	if( pthread_attr_setschedparam(&attr, &prio) != 0) 	  					  error(ERROR_PTRD_SETSCHED);
+	if( pthread_create(&control_presion, &attr, pressure_control, NULL) != 0) error(ERROR_PTRD_CREATE);
+
+	// #Sp
+	prio.sched_priority = 5;
+	if( pthread_attr_setschedparam(&attr, &prio) != 0) 	 				    error(ERROR_PTRD_SETSCHED);
+	if( pthread_create(&sensor_presion, &attr, pressure_sensor, NULL) != 0) error(ERROR_PTRD_CREATE);
+
+	pthread_join(monitor, NULL); // #M
+	pthread_join(control_temperatura, NULL); // #Ct
+	pthread_join(control_presion, NULL); // #Cp
+	pthread_join(sensor_presion, NULL); // #Sp
+	pthread_join(sensor_temperatura, NULL); // #St
 
 	return 0;
 }
@@ -161,7 +266,7 @@ int main() {
 
 
 /**
-* Funcion para inicializar la estacion, con los valores por defecto
+*	Funcion para inicializar la estacion, con los valores por defecto.
 *
 */
 void init_station() {
@@ -171,7 +276,7 @@ void init_station() {
 }
 
 /**
-* Funcion para inicializar los mutex que utilizaran las hebras
+*	Funcion para inicializar los mutex que utilizaran las hebras
 *
 */
 void configure_mutex() {
@@ -185,11 +290,13 @@ void configure_mutex() {
 	pthread_mutexattr_init(&attr_temp_mutex);
 	pthread_mutexattr_init(&attr_press_mutex);
 	
+	// temperatura
 	if( pthread_mutexattr_setprotocol(&attr_temp_mutex, PTHREAD_PRIO_PROTECT) != 0) error(ERROR_SET_PROTOCOL);
-	if( pthread_mutexattr_setprioceiling(&attr_temp_mutex, 3) != 0) error(ERROR_SET_PRIOCEILING);
+	if( pthread_mutexattr_setprioceiling(&attr_temp_mutex, 3) != 0) 			    error(ERROR_SET_PRIOCEILING);
 
+	// presion
 	if( pthread_mutexattr_setprotocol(&attr_press_mutex, PTHREAD_PRIO_PROTECT) != 0) error(ERROR_SET_PROTOCOL);
-	if( pthread_mutexattr_setprioceiling(&attr_press_mutex, 3) != 0) error(ERROR_SET_PRIOCEILING);
+	if( pthread_mutexattr_setprioceiling(&attr_press_mutex, 5) != 0) 				 error(ERROR_SET_PRIOCEILING);
 
 	pthread_mutex_init(&mutex_temp, &attr_temp_mutex);
 	pthread_mutex_init(&mutex_press, &attr_press_mutex);
@@ -201,7 +308,7 @@ void configure_mutex() {
 /**
 *	Control Presion. Funcion para el control de la presion; se usa en un hebra.
 *	Identificador: #Cp
-*	Prioridad: 2
+*	Prioridad: 4
 *	
 *	Utiliza clock_nanosleep para hacer los ciclos. Cambia el estado del sistema\
 *	para abrir/cerrar la valvula, haciendo que baje la presion.
@@ -220,10 +327,16 @@ void *pressure_control( void *args) {
 		pthread_mutex_lock(&mutex_press);
 
 		if (press > MAX_PRESSURE) {
+			// Abrimos la valvula para bajar la presion
 			state |= ACT_PRES;
+			get_actual_time();
+			printf("CONTROL DE PRESION: ABRIENDO la valvula de presion...\n");
 		}
 		else if (press > MIN_PRESSURE) {
+			// Cerramos la valvula de presion
 			state &= DEACT_PRES;
+			get_actual_time();
+			printf("CONTROL DE PRESION: CERRANDO la valvula de presion...\n");
 		}
 		ms_espera_activa(350); // TODO: ir a funciones para ver el estado
 
@@ -260,10 +373,18 @@ void *temperature_control( void *args) {
 		pthread_mutex_lock(&mutex_temp);
 
 		if (temp > MAX_TEMPERATURE) {
+			// Activamos refrigeracion
 			state |= ACT_TEMP;
+
+			get_actual_time();
+			printf("CONTROL DE TEMPERATURA: ACTIVADNO sistema de refrigeracion...\n");
 		}
 		else if (temp > MIN_TEMPERATURE) {
+			// Desactivamos refrigeracion
 			state &= DEACT_TEMP;
+
+			get_actual_time();
+			printf("CONTROL DE TEMPERATURA: DESACTIVANDO sistema de refrigeracion...\n");
 		}
 		ms_espera_activa(450); // TODO: ir a funciones para ver el estado
 
@@ -280,7 +401,7 @@ void *temperature_control( void *args) {
 /**
 *	Sensor Presion. Funcion para actualizar el valor de la presion; se usa en una hebra.
 *	Identificador: #Sp
-*	Prioridad: 3
+*	Prioridad: 5
 *
 *	Utiliza clock_nanosleep para hacer los ciclos. Modifica el valor del sensor\
 *	dependiendo de si esta o no abierta la valvula, aumentara o disminuira.
@@ -359,6 +480,17 @@ void *temperature_sensor( void *args) {
 
 }
 
+/**
+*	Monitorizacion del sistema. Muestra por pantalla los valores de los sensores y el estado de los actuadores.
+*	Identificador: #M
+*	Prioridad: 1
+*
+*	Utiliza clock_nanosleep para hacer los ciclos.
+*
+*	@mutex mutex_temp  Compartido con #St y #Ct 
+*	@mutex mutex_press Compartido con #Sp y #Cp
+*
+*/
 void *monitoring( void *args) {
 
 	struct timespec next_activation;
